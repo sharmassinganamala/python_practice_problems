f6b05e50c16a2a38689243aa8c35206f5f4377b5
sharmassinganamala <singanamalasharmasvali@gmail.com>
2024-05-24 07:40:27 +0530

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,8 @@
+# welcome to age checker for rooler coster , if the age is above 20 
+# they can ride ,if not they cant ride
+print("............Welcome to the roller coster............ ")
+age = int(input("how much your age : "))
+if age >= 20:
+    print("you can ride the roller coster!")
+else:
+    print("Sorry! you cannot ride the roller coster b/z your age is less than 20!")
