172da7bcb8ee66c07e807106cc1b250ec1604ccf
sharmassinganamala <singanamalasharmasvali@gmail.com>
2024-06-05 08:01:47 +0530

    Create Functions.py

---
+++ Functions.py
@@ -0,0 +1,21 @@
+#functions
+def my_function():
+    print("\nThis is for the first one")
+    print("Hello sharmas\n")
+my_function()
+
+#function with parameters
+def sharmas(name):
+    print("This is for second one")
+    print(f"how are you {name}\n")
+sharmas("Sharmas")
+
+#function with two parameters
+def singanamala(name,location):
+    print("and for the third parameters")
+    print(f"how old are you {name}")
+    print(f"can i know your location ? {location}\n")
+singanamala(name = "Sharmas",location = "Brahmanapalli")
+
+# here the name and location are the parameters
+# the sharmas and brahmanapalli are the arguments
